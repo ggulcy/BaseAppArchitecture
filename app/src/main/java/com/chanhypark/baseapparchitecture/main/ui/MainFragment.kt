558aaa8f259b4a24df28e05e721c9c4ab15ac8a9
chanhypark <chanhypark@ebay.com>
2020-10-31 04:13:03 +0900

    no message

--- app/src/main/java/com/chanhypark/baseapparchitecture/main/ui/MainFragment.kt
+++ app/src/main/java/com/chanhypark/baseapparchitecture/main/ui/MainFragment.kt
@@ -1,12 +1,16 @@
 package com.chanhypark.baseapparchitecture.main.ui
 
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.chanhypark.baseapparchitecture.R
 import com.chanhypark.baseapparchitecture.main.ui.view_model.MainViewModel
@@ -23,20 +27,33 @@ class MainFragment : Fragment() {
     ): View? = inflater.inflate(R.layout.fragmnet_main, container, false)
 
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         observeData()
 
+        val imm =activity?.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        tv_hint.setOnClickListener {
+            tv_hint.visibility = View.GONE
+            edit_text.requestFocus()
+            imm.showSoftInput(edit_text,0)
+        }
+
+
+        edit_text.setOnFocusChangeListener { view, b ->
+            if(b || !edit_text.text.isNullOrEmpty()) tv_hint.visibility = View.GONE
+            else tv_hint.visibility = View.VISIBLE
+        }
     }
 
 
     fun observeData() {
         mainViewModel.userList.observe(viewLifecycleOwner, Observer {
-            context?.let {context->
-                rv_user.adapter = UserAdapter(context).apply{
-                    collections = it
-                }
+            context?.let { context ->
+//                rv_user.adapter = UserAdapter(context).apply{
+//                    collections = it
+//                }
             }
         })
     }
