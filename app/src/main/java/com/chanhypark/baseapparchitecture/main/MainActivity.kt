6986211d61e0c78e873f2cc702b0c58ea9223234
chanhypark <chanhypark@ebay.com>
2021-03-10 01:51:42 +0900

    no message

--- app/src/main/java/com/chanhypark/baseapparchitecture/main/MainActivity.kt
+++ app/src/main/java/com/chanhypark/baseapparchitecture/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import androidx.activity.viewModels
+import com.bumptech.glide.Glide
 import com.chanhypark.baseapparchitecture.R
 import com.chanhypark.baseapparchitecture.app.BaseActivity
 import com.chanhypark.baseapparchitecture.main.custom_view.CustomView
@@ -18,10 +19,10 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mainViewModel.getUser()
+//        mainViewModel.getUser()
         observeData()
 
-        container.addView(CustomView2(this))
+//        container.addView(CustomView2(this))
 //        LinearLayout mEmailLinear = (LinearLayout)getView().findViewById(R.id. 추가할 위치의 LinearLayout id );
 //        LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 //        View emailNewField = inflater.inflate(R.layout. 추가할 뷰의 layout xml 파일명 , null);
@@ -31,6 +32,9 @@ class MainActivity : BaseActivity() {
 //        val view = LayoutInflater.from(this).inflate(R.layout.item_custom,null)
 
 //        container.addView(view)
+
+        Glide.with(this).load("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.instiz.net%2Fname_enter%2F57854833%3Ffrompc%3D1&psig=AOvVaw3hrYirMObI-BISD26SCSv0&ust=1613699004563000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOi8sbKn8u4CFQAAAAAdAAAAABAD").into(iv_picture)
+        Glide.with(this).load("https://www.google.com/url?sa=i&url=https%3A%2F%2Ftwitter.com%2F_travelkim%2Fstatus%2F1175946872541704192&psig=AOvVaw06ofYGR7ci-iIsJFUtZU6o&ust=1613699068526000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCICX29Gn8u4CFQAAAAAdAAAAABAD").into(iv_picture1)
 
     }
 
