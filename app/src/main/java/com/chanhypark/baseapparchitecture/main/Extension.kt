2000119997911eb2fd6401571939989362c3cb90
chanhypark <chanhypark@ebay.com>
2021-04-13 10:52:29 +0900

    statusbar test

--- app/src/main/java/com/chanhypark/baseapparchitecture/main/Extension.kt
+++ app/src/main/java/com/chanhypark/baseapparchitecture/main/Extension.kt
@@ -1,9 +1,16 @@
 package com.chanhypark.baseapparchitecture.main
 
+import android.content.Context
+import android.os.Build
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import com.chanhypark.baseapparchitecture.R
 
 fun AppCompatActivity.replaceFragmentInActivity(fragment: Fragment, frameId: Int) {
     supportFragmentManager.transact {
@@ -12,16 +19,16 @@ fun AppCompatActivity.replaceFragmentInActivity(fragment: Fragment, frameId: Int
 }
 
 
-fun AppCompatActivity.addFragment(fragment: Fragment, frameId: Int){
+fun AppCompatActivity.addFragment(fragment: Fragment, frameId: Int) {
     supportFragmentManager.transact {
-        add(frameId,fragment)
+        add(frameId, fragment)
     }
 }
 
 
-fun AppCompatActivity.addFragmentWithStack(fragment: Fragment, frameId: Int, name:String? = null){
+fun AppCompatActivity.addFragmentWithStack(fragment: Fragment, frameId: Int, name: String? = null) {
     supportFragmentManager.transact {
-        add(frameId,fragment)
+        add(frameId, fragment)
         addToBackStack(name)
     }
 }
@@ -30,4 +37,23 @@ private inline fun FragmentManager.transact(action: FragmentTransaction.() -> Un
     beginTransaction().apply {
         action()
     }.commit()
+}
+
+
+fun Window.setStatusBarColorCustom(colorId: Int, isLightStatusBar: Boolean) {
+    if (Build.VERSION.SDK_INT >= 23) {
+        this.apply {
+            statusBarColor = ContextCompat.getColor(context, colorId)
+            if (isLightStatusBar) decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+    } else {
+//        this.apply {
+//            statusBarColor = ContextCompat.getColor(context, colorId)
+//            if (isLightStatusBar) {
+//                clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+//                addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+//            }
+//        }
+    }
 }
--- app/src/main/java/com/chanhypark/baseapparchitecture/main/ui/MainActivity.kt
+++ app/src/main/java/com/chanhypark/baseapparchitecture/main/ui/MainActivity.kt
@@ -2,10 +2,10 @@ package com.chanhypark.baseapparchitecture.main.ui
 
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.Observer
 import com.chanhypark.baseapparchitecture.R
 import com.chanhypark.baseapparchitecture.app.BaseActivity
 import com.chanhypark.baseapparchitecture.main.replaceFragmentInActivity
+import com.chanhypark.baseapparchitecture.main.setStatusBarColorCustom
 import com.chanhypark.baseapparchitecture.main.ui.view_model.MainViewModel
 
 class MainActivity : BaseActivity() {
@@ -17,6 +17,28 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
 
         mainViewModel.getUser()
+
+
+
+        this.window.setStatusBarColorCustom(R.color.colorRed,false)
+
+//
+//        if(Build.VERSION.SDK_INT >= 23){
+//            window.apply {
+////                addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+//                statusBarColor = ContextCompat.getColor(context,R.color.colorYellow)
+//                decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+//            }
+//        }else{
+//            window.apply {
+//                clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+//                addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+//                statusBarColor = ContextCompat.getColor(context,R.color.colorYellow)
+//            }
+//        }
+
+
+
 
         replaceFragmentInActivity(MainFragment.newInstance(),R.id.layout_fr)
 //        observeData()
